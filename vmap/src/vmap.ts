f31a08e17d6890900a4dc79ccf1ce5900377adc1
Chris Clark <chris.clark876@gmail.com>
2020-06-23 19:39:51 +0100

    sxc promise return

--- vmap/src/vmap.ts
+++ vmap/src/vmap.ts
@@ -9,35 +9,33 @@ const s3 = new AWS.S3({
 });
 const vastClient = new VASTClient();
 
-const transcodeAd = (creativeId: string, adUrl: string) => {
-    return new Promise(async (res, rej) => {
-        console.log("Hai");
-        const ffmpegOutput = spawn('../ffmpeg/ffmpeg-bin/ffmpeg', [
-            '-i',
-            adUrl,            
-            '-c',
-            'copy',
-            '-f',
-            'mpegts',
-            '../ads/ad.ts'
-        ]);
+const transcodeAd = (creativeId: string, adUrl: string) => new Promise((res, rej) => {
+    console.log("Hai");
+    const ffmpegOutput = spawn('../ffmpeg/ffmpeg-bin/ffmpeg', [
+        '-i',
+        adUrl,
+        '-c',
+        'copy',
+        '-f',
+        'mpegts',
+        '../ads/ad.ts'
+    ]);
 
-        ffmpegOutput.stderr.on('data', data => console.log(data.toString()));        
+    ffmpegOutput.stderr.on('data', data => console.log(data.toString()));
 
-        ffmpegOutput.stdout.on('close', async () => {
-            const params = {
-                Bucket: 'hboremixbucket',
-                Key: `ads/${creativeId}.ts`,
-                Body: fs.readFileSync('../ads/ad.ts')
-            }
-            
-            const upload = await s3.upload(params).promise();
-            console.log(upload);
-            spawn('rm', ['../ads/ad.ts']);
-            res();
-        });        
-    })
-}
+    ffmpegOutput.stdout.on('close', async () => {
+        const params = {
+            Bucket: 'hboremixbucket',
+            Key: `ads/${creativeId}.ts`,
+            Body: fs.readFileSync('../ads/ad.ts')
+        }
+
+        const upload = await s3.upload(params).promise();
+        console.log(upload);
+        spawn('rm', ['../ads/ad.ts']);
+        res();
+    });
+});
 
 export default transcodeAd;
 
